ce29a9b951075f934669180f32c340b5c4eb03f4
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2023-10-24 22:42:42 +0800

    modified accounts controller

--- pos_sales/src/main/java/com/pos_sales/controller/AccountsController.java
+++ pos_sales/src/main/java/com/pos_sales/controller/AccountsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.pos_sales.service.AccountsService;
 
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/user")
 public class AccountsController {
 
--- pos_sales/src/main/java/com/pos_sales/repository/AccountsRepository.java
+++ pos_sales/src/main/java/com/pos_sales/repository/AccountsRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pos_sales.model.AccountsModel;
+import com.pos_sales.model.TransactionModel;
 
 @Repository
 public interface AccountsRepository extends JpaRepository<AccountsModel, Integer>{
 		AccountsModel findByUsername(String username); 
+		AccountsModel findByUserid(int userid); 
 }
