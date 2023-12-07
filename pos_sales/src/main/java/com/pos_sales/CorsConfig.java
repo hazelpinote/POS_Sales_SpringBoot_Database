ef21570b8244aaa7ec6381fada1e7d72f7f9b4c5
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2023-12-07 13:49:12 +0800

    Update CorsConfig.java

--- pos_sales/src/main/java/com/pos_sales/CorsConfig.java
+++ pos_sales/src/main/java/com/pos_sales/CorsConfig.java
@@ -1,23 +1,15 @@
-package com.pos_sales;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://dilven-pos.vercel.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
-
